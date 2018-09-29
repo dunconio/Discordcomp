a881b20d8a6f7f2f032ad703eac0a432d3e873b8
dunconio <30807904+dunconio@users.noreply.github.com>
2018-09-29 10:40:08 +0100

    Updated dummy program now just sleeps
    We rely on the script threads to kill the .exe.dummy programs, instead of quitting after 5 minutes.

--- dummy.c
+++ dummy.c
@@ -1,11 +1,9 @@
-#include <stdio.h> // standard I/O header file
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
-printf("\nKill me when done !!!\n"); // print "Hello World" message on screen
 
-int i;
-for (i = 0; i < 10;)
+while(1)
 {
 
 sleep(30);
